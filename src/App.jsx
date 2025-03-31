ba4470d2d759b96c9f1cca640a087734637a8e90
Kaballah <kabzkabala4@gmail.com>
2025-03-31 12:47:07 +0300

    Git Ignore file

---
+++ src/App.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <section id="section1" className="full-height-section">
+        <h1>Section 1</h1>
+      </section>
+      <section id="section2" className="full-height-section">
+        <h1>Section 2</h1>
+      </section>
+      <section id="section3" className="full-height-section">
+        <h1>Section 3</h1>
+      </section>
+      <section id="section4" className="full-height-section">
+        <h1>Section 4</h1>
+      </section>
+      <section id="section5" className="full-height-section">
+        <h1>Section 5</h1>
+      </section>
+      <section id="section6" className="full-height-section">
+        <h1>Section 6</h1>
+      </section>
+    </div>
+  );
+}
+
+export default App;
